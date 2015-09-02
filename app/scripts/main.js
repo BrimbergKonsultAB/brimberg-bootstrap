7ff94fd6804f2c253ca163756e493ffa0fb104cc
jens brimberg <jens.brimberg@gmail.com>
2015-09-02 20:51:13 +0200

    Making a 2x3 grid with nav buttons...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,9 @@
 // jshint devel:true
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function () {
+    console.log('page ready');
+    $('#myOne').stellar();
+    $('#myTwo').stellar();
+    $('#myThree').stellar();
+});
