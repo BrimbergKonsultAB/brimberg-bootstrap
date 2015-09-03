5f5fe2286c9b013ea76ec0df5cda6eb15fa3507e
jens brimberg <jens.brimberg@gmail.com>
2015-09-03 20:23:21 +0200

    Added content..

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,27 @@
-// jshint devel:true
-console.log('\'Allo \'Allo!');
+var langOptions = ['swedish', 'english'],
+    lang = langOptions[0];
+
+function rotateLanguage() {
+    if (langOptions.indexOf(lang) >= langOptions.length - 1) {
+        lang = langOptions[0];
+    } else {
+        lang = langOptions[langOptions.indexOf(lang) + 1];
+    }
+
+    for (var l in langOptions) {
+        var lStr = langOptions[l];
+        if (lStr === lang) {
+            $('.' + lStr).show();
+        } else {
+            $('.' + lStr).hide();
+        }
+    }
+}
 
 $(document).ready(function () {
-    console.log('page ready');
-    $('#myOne').stellar();
-    $('#myTwo').stellar();
-    $('#myThree').stellar();
+    $('.' + langOptions[1]).hide();
+
+    $(".glyph-lang-image").on('click', function () {
+        rotateLanguage();
+    });
 });
