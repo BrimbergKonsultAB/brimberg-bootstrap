925546eb021b6e395ce99a99c010aaadf9c808ff
U-AZA\jenbri <jens.brimberg@gmail.com>
2017-05-05 14:21:32 +0200

    Edited timeout options

--- test/spec/test.js
+++ test/spec/test.js
@@ -2,7 +2,6 @@
   'use strict';
 
   describe('Give it some context', function () {
-    this.timeout(5000);
     describe('maybe a bit more context here', function () {
       it('should run here few assertions', function () {
 
