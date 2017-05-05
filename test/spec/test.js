a186b69a96cdca9eb2f10a1a770a86027a8de5c1
U-AZA\jenbri <jens.brimberg@gmail.com>
2017-05-05 14:05:31 +0200

    Set timeout for test

--- test/spec/test.js
+++ test/spec/test.js
@@ -2,6 +2,7 @@
   'use strict';
 
   describe('Give it some context', function () {
+    this.timeout(5000);
     describe('maybe a bit more context here', function () {
       it('should run here few assertions', function () {
 
